c385d58619ca9bcb37ca40fbaa7b5b980e608c26
Camilo2B <camilo.ruizl@uqvirtual.edu.co>
2025-09-22 18:34:58 -0500

    pruebas y pilas ej 1 y 6 y ejercicio 3

--- ej3_taller.exs
+++ ej3_taller.exs
@@ -2,24 +2,22 @@
 
 defmodule SumaTransacciones do
 
-  def suma_transacciones([]) do ## Caso base
-  [[]]
-  end
 
-  def suma_transacciones(lista) do
-
+  def sumar_con_acumulador(transacciones) do
+    sumar_con_acumulador(transacciones, 0)
   end
 
 
+  def sumar_con_acumulador([], acumulador), do: acumulador
 
-
-
-
-
-
+  def sumar_con_acumulador([head | tail], acumulador) do
+    sumar_con_acumulador(tail, acumulador + head)
+  end
 
   def main do
-    balance_final = suma_transacciones([23000, -10000, 8000])
+    balance_final = sumar_con_acumulador([23000, -10000, 8000])
     Util.show_message("El balance final es de #{balance_final}")
   end
 end
+
+SumaTransacciones.main()
